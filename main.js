916d54a6e8c61c3bb9aeb4c15ba4a40dfa4f6d85
Stephen Woosley <swoosley@sharptop.co>
2017-01-10 21:33:20 -0500

    got data saving and populating correctly

--- main.js
+++ main.js
@@ -17,16 +17,58 @@
 
 var database = firebase.database();
 
+var currentName = "";
+var currentRole = "";
+var currentDate = "";
+var currentRate = "";
+
+
 // ------------------------------------------------------------
 
 // Firebase initial load stuff
 
-database.ref().on("value", function(snapshot) {
+database.ref().on("child_added", function(childSnapshot) {
+	
+	// save the value of the fb db childSnapshot to var loadData
+	var loadData = childSnapshot.val();
+	// save the keys for each value in the childSnapshot to var loadDataKeys
+	var loadDataKeys = Object.keys(loadData);
+	// get the count of how many keys there are, then subtract 1 to get the index of the last object pushed to the database.
+	var lastObjectIndex = loadDataKeys.length-1;
+	// get the key name/object for the 
+	// var lastObjectKey = loadDataKeys[lastObjectIndex];
+	// var lastObject = loadData[lastObjectKey];
 
-	var save = snapshot.val();
+	console.log(childSnapshot);
+	console.log("loadData: "+loadData);
+	console.log("loadData value at 0: "+ loadData[0]);
+	console.log("loadDataKeys: "+loadDataKeys);
+	console.log("lastObjectIndex: "+lastObjectIndex);
+	console.log(loadData.name);
+	// console.log("lastObjectKey: "+lastObjectKey);
+	// console.log("lastObject: "+lastObject);
+	// console.log("name property of last object: "+lastObject.name);
 
-	saveArray = Object.keys(save);
+	var employeeTable = $("#employee-table");
+	
+	//for id column, will have to calc total existing employees
+	employeeTable.append("<tr>"+
+	      "<td>1</td>"+ 
+	      "<td>"+ loadData.name +"</td>"+
+	      "<td>"+ loadData.role +"</td>"+
+	      "<td>"+ loadData.date +"</td>"+
+	      "<td>Months Worked</td>"+
+	      "<td>$ "+ loadData.rate +"</td>"+
+	      "<td>Total Billed</td>"+
+	    "</tr>"
+	);
 
+	if (!currentName && !currentRole && !currentDate && !currentRate) {
+		console.log("no values, so need a loop to print tables to page.")
+		//for (things in stuff) {
+
+		}
+	
 
 }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
@@ -39,31 +81,34 @@ database.ref().on("value", function(snapshot) {
 
 $(document).on("click", "#submit-new", function(e) {
 	e.preventDefault();
-	var currentName = $("#inputName").val().trim();
-	var currentRole = $("#inputRole").val().trim();
-	var currentDate = $("#inputDate").val().trim();
-	var currentRate = $("#inputRate").val().trim();
+	currentName = $("#inputName").val().trim();
+	currentRole = $("#inputRole").val().trim();
+	currentDate = $("#inputDate").val().trim();
+	currentRate = $("#inputRate").val().trim();
 
-	database.ref().push({
-      "name" : currentName,
-      "role" : currentRole,
-      "date" : currentDate,
-      "rate" : currentRate
-    })
+	var tempEmpl = {
+		name: currentName,
+		role: currentRole,
+		date: currentDate,
+		rate: currentRate
+	};
+
+	database.ref().push(tempEmpl);
+
 
 	var employeeTable = $("#employee-table");
-	console.log("Submit clicked with: "+currentName+" "+currentRole+" "+currentDate+ " "+currentRate);
+	console.log("Submit clicked with: "+tempEmpl.name+" "+tempEmpl.role+" "+tempEmpl.date+ " "+tempEmpl.rate);
 	//for id column, will have to calc total existing employees
-	employeeTable.append("<tr>"+
-	      "<td>1</td>"+ 
-	      "<td>"+ currentName +"</td>"+
-	      "<td>"+ currentRole +"</td>"+
-	      "<td>"+ currentDate +"</td>"+
-	      "<td>Months Worked</td>"+
-	      "<td>$ "+ currentRate +"</td>"+
-	      "<td>Total Billed</td>"+
-	    "</tr>"
-	);
+	// employeeTable.append("<tr>"+
+	//       "<td>1</td>"+ 
+	//       "<td>"+ currentName +"</td>"+
+	//       "<td>"+ currentRole +"</td>"+
+	//       "<td>"+ currentDate +"</td>"+
+	//       "<td>Months Worked</td>"+
+	//       "<td>$ "+ currentRate +"</td>"+
+	//       "<td>Total Billed</td>"+
+	//     "</tr>"
+	// );
 
 	$("#inputName").val("");
 	$("#inputRole").val("");
